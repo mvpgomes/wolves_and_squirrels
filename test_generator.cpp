9ddd92e234a6732767301533ee51bdb61a2a7311
mvpgomes <mvp.gomes1989@gmail.com>
2013-10-29 18:38:08 +0000

    Program that generates a input file for testing wolves_and_squirrels, the program receives as parameters the size (N) of the matrix side and the number of elements (M) that we want fill the matrix.

---
+++ test_generator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <map>
+using namespace std;
+/*main function*/
+int main(int argc, char **argv){
+  int size, n_elements, row, column;
+  char element;
+  map<int,int> keys;
+  map<int,int>::iterator it;
+  ofstream file;
+
+  char array[] = {'i','s','t','w'};
+
+  if( argc != 3 ){
+    cout << "Wrong number of arguments!" << endl;
+    exit(-1);
+  } else { 
+    size = atoi(argv[1]);
+    n_elements = atoi(argv[2]);
+  }
+  if( n_elements > size * size ){
+    cout << "Number of elements exceed the allowed!" << endl;
+  } else { file.open("test.txt"); }
+
+  file << size << endl;
+  int i = 0;
+  while( i < n_elements ){
+    // random generator for the positions and elements
+    row = rand() % size;
+    column = rand() % size;
+    element = array[rand()%4];
+    if( keys.find(row)->second != column){
+      keys.insert(make_pair(row,column));
+      file << row << " " << column << " " << element << endl;
+      i++;
+    }
+  }
+  file.close();
+  return 0;
+}
